291c04841fc648bb0eaad7b757ab19086a7a07ce
Harry <harryfisher21@gmail.com>
2022-01-10 20:04:19 +0000

    rename src file and move favicon to www/ after build

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -2,6 +2,7 @@ options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mo
 options(shiny.port = httpuv::randomPort())
 # Could include this to trigger build when running `golem::run_dev()`
 system("yarn build")
+system("mv inst/app/favicon.ico inst/app/www/")
 golem::detach_all_attached()
 golem::document_and_reload()
 run_app()
